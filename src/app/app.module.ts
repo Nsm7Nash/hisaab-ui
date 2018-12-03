a23e276a1c1057e6fc698ec4e29441a61436c28c
Nainesh <nainesh2m@gmail.com>
2018-12-02 16:36:40 -0800

    Add Login and home boilerPlate.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component : LoginComponent
+      },
+      {
+        path: '',
+        component : HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
